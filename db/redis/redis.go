1096c1852d6da1fc669aed3b3b4c8889741c441f
hongjie <32968210@qq.com>
2020-03-01 13:00:51 +0800

    增加了redis

--- db/redis/redis.go
+++ db/redis/redis.go
@@ -12,14 +12,14 @@ type DialContext struct {
 }
 
 // Dial 连接
-func Dial(host string, db int) *DialContext {
+func Dial(host string, db, maxIdle, maxActive int) *DialContext {
 	c := &DialContext{}
 	// 建立连接池
 	c.redisClient = &red.Pool{
 		// 最大的空闲连接数，表示即使没有redis连接时依然可以保持N个空闲的连接，而不被清除，随时处于待命状态。
-		MaxIdle: 1,
+		MaxIdle: maxIdle,
 		// 最大的激活连接数，表示同时最多有N个连接
-		MaxActive: 10,
+		MaxActive: maxActive,
 		// 最大的空闲连接等待时间，超过此时间后，空闲连接将被关闭
 		IdleTimeout: 180 * time.Second,
 		Wait:        true,
