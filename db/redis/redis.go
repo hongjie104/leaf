bcfea4da94df75848848e1eaf94782787776d247
hongjie <32968210@qq.com>
2020-03-01 12:52:07 +0800

    增加了redis

--- db/redis/redis.go
+++ db/redis/redis.go
@@ -6,19 +6,16 @@ import (
 	red "github.com/garyburd/redigo/redis"
 )
 
-var (
-	redisClient *red.Pool
-)
-
 // DialContext DialContext
 type DialContext struct {
+	redisClient *red.Pool
 }
 
 // Dial 连接
-func Dial(host string, db int) {
-	// 从配置文件获取redis的ip以及db
+func Dial(host string, db int) *DialContext {
+	c := &DialContext{}
 	// 建立连接池
-	redisClient = &red.Pool{
+	c.redisClient = &red.Pool{
 		// 最大的空闲连接数，表示即使没有redis连接时依然可以保持N个空闲的连接，而不被清除，随时处于待命状态。
 		MaxIdle: 1,
 		// 最大的激活连接数，表示同时最多有N个连接
@@ -43,9 +40,10 @@ func Dial(host string, db int) {
 			return c, nil
 		},
 	}
+	return c
 }
 
 // Ref Ref
 func (c *DialContext) Ref() red.Conn {
-	return redisClient.Get()
+	return c.redisClient.Get()
 }
