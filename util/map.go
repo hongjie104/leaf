b762d263b23b14a30ac9f30b412c3e2ac308b057
hongjie <32968210@qq.com>
2020-12-21 22:18:19 +0800

    clean up

--- util/map.go
+++ util/map.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 )
 
+// Map Map
 type Map struct {
 	sync.RWMutex
 	m map[interface{}]interface{}
@@ -15,31 +16,35 @@ func (m *Map) init() {
 	}
 }
 
+// UnsafeGet UnsafeGet
 func (m *Map) UnsafeGet(key interface{}) interface{} {
 	if m.m == nil {
 		return nil
-	} else {
-		return m.m[key]
 	}
+	return m.m[key]
 }
 
+// Get Get
 func (m *Map) Get(key interface{}) interface{} {
 	m.RLock()
 	defer m.RUnlock()
 	return m.UnsafeGet(key)
 }
 
+// UnsafeSet UnsafeSet
 func (m *Map) UnsafeSet(key interface{}, value interface{}) {
 	m.init()
 	m.m[key] = value
 }
 
+// Set Set
 func (m *Map) Set(key interface{}, value interface{}) {
 	m.Lock()
 	defer m.Unlock()
 	m.UnsafeSet(key, value)
 }
 
+// TestAndSet TestAndSet
 func (m *Map) TestAndSet(key interface{}, value interface{}) interface{} {
 	m.Lock()
 	defer m.Unlock()
@@ -48,58 +53,66 @@ func (m *Map) TestAndSet(key interface{}, value interface{}) interface{} {
 
 	if v, ok := m.m[key]; ok {
 		return v
-	} else {
-		m.m[key] = value
-		return nil
 	}
+	m.m[key] = value
+	return nil
 }
 
+// UnsafeDel UnsafeDel
 func (m *Map) UnsafeDel(key interface{}) {
 	m.init()
 	delete(m.m, key)
 }
 
+// Del Del
 func (m *Map) Del(key interface{}) {
 	m.Lock()
 	defer m.Unlock()
 	m.UnsafeDel(key)
 }
 
+// UnsafeLen UnsafeLen
 func (m *Map) UnsafeLen() int {
 	if m.m == nil {
 		return 0
-	} else {
-		return len(m.m)
 	}
+	return len(m.m)
 }
 
+// Len Len
 func (m *Map) Len() int {
 	m.RLock()
 	defer m.RUnlock()
 	return m.UnsafeLen()
 }
 
-func (m *Map) UnsafeRange(f func(interface{}, interface{})) {
+// UnsafeRange UnsafeRange
+func (m *Map) UnsafeRange(f func(interface{}, interface{}) bool) {
 	if m.m == nil {
 		return
 	}
 	for k, v := range m.m {
-		f(k, v)
+		if f(k, v) {
+			break
+		}
 	}
 }
 
-func (m *Map) RLockRange(f func(interface{}, interface{})) {
+// RLockRange RLockRange
+func (m *Map) RLockRange(f func(interface{}, interface{}) bool) {
 	m.RLock()
 	defer m.RUnlock()
 	m.UnsafeRange(f)
 }
 
-func (m *Map) LockRange(f func(interface{}, interface{})) {
+// LockRange LockRange
+func (m *Map) LockRange(f func(interface{}, interface{}) bool) {
 	m.Lock()
 	defer m.Unlock()
 	m.UnsafeRange(f)
 }
 
+// Reset Reset
 func (m *Map) Reset() {
 	m.m = make(map[interface{}]interface{})
 }
